45c6d098835e940d7b7c68c35dbb51f87f5f371e
nehashah4590 <ns457989@gmail.com>
2022-10-18 20:45:29 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+let circleTurn
+
+function startGame(){
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once : true})
+    })
+    setBoardHoverClass()
+}
+
+
+function handleClick(e){
+    //placeMark
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    //check For Win
+    //check for Draw
+    //switch Turns
+    swapTurns()
+    setBoardHoverClass()
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurn(){
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass(){
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if(circleTurn){
+        board.classList.add(CIRCLE_CLASS)
+
+    }
+    else{
+        board.classList.add(X_CLASS)
+    }
+}
